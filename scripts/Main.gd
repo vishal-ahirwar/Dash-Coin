3aa4eb6fa4da48aa4a9a00756d52fd8bdfe70fd8
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2022-10-25 17:42:48 +0530

    SpawnCoins

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -2,13 +2,17 @@ extends "res://scripts/gamemode.gd"
 
 export (PackedScene) var Coin
 export (int) var PlayTime
+export (float) var Difficulty
+var ScreenSize:Vector2
 
 func _ready():
 	randomize()
-	$Player.ScreenSize=get_viewport().get_visible_rect().size
+	ScreenSize=get_viewport().get_visible_rect().size
+	$Player.ScreenSize=ScreenSize
 	$Player.hide()
-	
+	NewGame()
 
+	
 func NewGame():
 	Playing=true
 	Level=1
@@ -19,8 +23,13 @@ func NewGame():
 	$GameTimer.start()
 	SpawnCoins()
 
+
 func SpawnCoins():
-	pass
+	for i in range(Difficulty*Level):
+		var c= Coin.instance()
+		$CoinContainer.add_child(c)
+		c.position=Vector2(rand_range(0,ScreenSize.x),rand_range(0,ScreenSize.y))
+
 
 
 	
